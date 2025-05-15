8e24e773878feee7a9ed4055b7bf76cf8a06418c
nimeullaha71 <nimeullahacse@gmail.com>
2025-05-15 22:46:16 +0600

    ToDo List

--- lib/Home.dart
+++ lib/Home.dart
@@ -8,21 +8,62 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int _count = 0;
-  incrementCount(){
-    setState(() {
-      _count++;
-    });
 
+  List<String> _task = [];
+  TextEditingController _taskController = TextEditingController();
+
+  _addTask(){
+    if (_taskController.text.isNotEmpty){
+      setState(() {
+        _task.add(_taskController.text);
+        _taskController.clear();
+      });
+    }
   }
+
+  _removeTask(int index){
+    setState(() {
+      _task.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text(_count.toString(),style: TextStyle(fontSize: 60,fontWeight: FontWeight.bold,color: Colors.green),),
+      appBar: AppBar(
+        title: Text("Simple TO DO List",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),),
+        backgroundColor: Colors.deepPurpleAccent,
+
       ),
-      floatingActionButton: FloatingActionButton(onPressed: incrementCount,child: Icon(Icons.add,size: 30,),),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _taskController,
+              decoration: InputDecoration(
+                hintText: "Enter Your Task",
+                border: OutlineInputBorder(),
+                suffixIcon: IconButton(onPressed: _addTask, icon: Icon(Icons.add))
+              ),
+            ),
+            SizedBox(height: 10,),
+            Expanded(child:
+            ListView.builder(
+                itemCount: _task.length,
+                itemBuilder: (context,index){
+                  return Card(
+                   child: ListTile(
+                     title: Text(_task[index]),
+                     trailing: IconButton(onPressed: ()=> _removeTask(index), icon: Icon(Icons.delete,color: Colors.red,)),
+                   ), 
+                  );
+                }
+            )
+            )
+          ],
+        ),
+      )
     );
   }
 }
